c0c630bf559837b15bf5fbae680c61e4a54d8c62
Tearies <maojun_huier@sina.com>
2019-05-07 16:26:41 +0800

    格式化代码

--- Tests/Microsoft.Dynamic.Test/ResourceManagerTest.cs
+++ Tests/Microsoft.Dynamic.Test/ResourceManagerTest.cs
@@ -3,15 +3,14 @@ using System.Threading;
 using Microsoft.Scripting;
 using NUnit.Framework;
 
-namespace Microsoft.Dynamic.Test
-{
+namespace Microsoft.Dynamic.Test {
     [TestFixture]
     public class ResourceManagerTest {
         [Test]
         public void Test() {
             ResourceManager.Default.Initialize("IronPython");
-          var aa=  ResourceManager.Default.GetResource("NotNull","this field is required!!");
-           var bb= ResourceManager.Default.GetResource("Overflow", "Overflow!!");
+            ResourceManager.Default.GetResource("NotNull", "this field is required!!");
+            ResourceManager.Default.GetResource("Overflow", "Overflow!!");
             ResourceManager.Default.SaveAs(CultureInfo.CurrentCulture);
         }
     }
